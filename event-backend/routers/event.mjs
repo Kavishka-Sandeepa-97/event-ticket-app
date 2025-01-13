f9caaabf07dc097a4604231998744bd88d926e56
Kavishka Sandeepa <kavishkapk99@gmail.com>
2025-01-13 11:58:27 +0530

    kk

--- event-backend/routers/event.mjs
+++ event-backend/routers/event.mjs
@@ -1,7 +1,13 @@
 import express from "express"
+import multer from "multer"
 
 const eventRoutes=express.Router();
+const storage=multer.memoryStorage();
+const upload=multer({storage});
 
-//eventRoutes.get();
+
+eventRoutes.post((req,res,next)=>{
+   const {} = req.body
+});
 
 export default eventRoutes;
